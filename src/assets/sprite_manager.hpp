4de41b5b824ece9ed9f51853fb85877d70ed6ffa
HexPrism <bharathkrishnan33@gmail.com>
2024-05-17 21:40:59 +0530

    Added assets folder

---
+++ src/assets/sprite_manager.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include "raylib/raylib.h"
+#include <cstdint>
+#include <data_types.hpp>
+#include <unordered_map>
+#include "../core/entity.hpp"
+
+#define DEBUG 
+
+namespace Core{
+
+class SpriteManager{
+private:
+    static std::unordered_map<const char *, Texture2D> loaded_textures;
+
+public:
+    static void loadTexture(const char* label,const char* path);
+    static void loadTexture(const char* label,Image* image);
+    static Texture2D* getTexture(const char* label);
+
+    static bool searchTexture(const char* label);
+
+    static void Destroy();
+};
+
+
+
+
+};
+
+
+class SpriteSheet{
+private:
+    vec2 initial_padding;
+    vec2 stride;
+    bool loop;
+    int curr_frame;
+    int max_frame;
+    int frame_speed;
+    int frame_counter = 0;
+    Texture2D* tex;
+
+    SpriteSheet(const char* label,vec2 initial_padding,int max_frame,int num_row,int num_rows,bool loop);
+public:
+    SpriteSheet();
+    static SpriteSheet CreateSpriteSheet(const char* label,vec2 initial_padding,int max_frame,int num_row,int num_rows,bool loop = false);
+    void AnimateFrame(transform& transform);
+    inline void ChangeFPS(int fps){
+        frame_speed = fps;
+    }
+    inline int getFPS(){
+        return frame_speed;
+    }
+    void DrawFrame(int frame,transform& transform);
+};
